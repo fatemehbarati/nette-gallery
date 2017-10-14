b9d839c356c1c32b05ce84842eaf369a0ea7ead5
fatemehbarati <barati.it@tezlabs.com>
2017-10-14 16:11:42 +0330

    add Listener for signInForm::OnSuccess

--- app/AdminModule/forms/SignInForm.php
+++ app/AdminModule/forms/SignInForm.php
@@ -4,9 +4,10 @@ namespace App\AdminModule\Forms;
 use App\AdminModule\Forms\Interfaces\ISignInFormFactory;
 use Nette\Application\Application;
 use Nette\Application\UI\Form;
+use Nette\Object;
 use Nette\Security\AuthenticationException;
 
-class SignInForm implements ISignInFormFactory
+class SignInForm extends Object implements ISignInFormFactory
 {
 
     /** @param $application Application */
@@ -14,6 +15,9 @@ class SignInForm implements ISignInFormFactory
     {
         $this->application = $application;
     }
+
+    /** @var  array */
+    public $onSuccess;
 
     /**
      * @return Form
@@ -39,9 +43,12 @@ class SignInForm implements ISignInFormFactory
     public function signInOnSucceeded(Form $form, $values)
     {
         try{
+
             $this->application->getPresenter()->getUser()->login($values->username, $values->password);
+            $this->onSuccess($values->username, $values->password);
             $this->application->getPresenter()->redirect(':Admin:Homepage:');
         }catch (AuthenticationException $exception){
+
             $form->addError($exception->getMessage());
         }
     }
---
+++ app/listener/SignInMailerListener.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Listener;
+
+use Kdyby\Events\Subscriber;
+use Nette\Object;
+
+class SignInMailerListener extends Object implements Subscriber
+{
+    public function onSuccess($username, $password)
+    {
+        /** Do what you want to do */
+        
+    }
+    public function getSubscribedEvents()
+    {
+        return array('App\AdminModule\Forms\SignInForm::onSuccess' => 'onSuccess');
+    }
+}
